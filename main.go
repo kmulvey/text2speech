1182164856390e52997bd7e76ca0dc2406c4cc16
Kevin Mulvey <kmulvey@linux.com>
2022-07-26 19:36:17 -0600

    ffmpeg

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"net/url"
 	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -43,7 +47,7 @@ func main() {
 	flag.StringVar(&awsRegion, "region", "us-west-2", "aws region to use")
 	flag.StringVar(&voiceID, "voice", "Matthew", "voice to use")
 	flag.StringVar(&inputFile, "input", "", "path the input text file, if this is specified STDIN will be ignored")
-	flag.StringVar(&outputFile, "output", "", "path the save the mp3, this will NOT play the audio")
+	flag.StringVar(&outputFile, "output", "speech.mp3", "path the save the mp3, this will NOT play the audio")
 
 	flag.Parse()
 
@@ -102,10 +106,21 @@ func main() {
 		log.Fatalf("error from synthesisText: %v", err)
 	}
 
+	// get the length
+	if err := writeFile(voice.Body, outputFile); err != nil {
+		log.Fatalf("error writing file %v", err)
+	}
+	length, err := getLength(outputFile)
+	fmt.Println(length, err)
+	if err != nil {
+		log.Fatalf("error getting length from ffmpeg: %v", err)
+	}
+	os.Exit(0)
+
 	// output switch
-	if strings.TrimSpace(outputFile) != "" {
-		if err := writeFile(voice.Body, outputFile); err != nil {
-			log.Fatalf("error writing file %v", err)
+	if strings.TrimSpace(outputFile) == "" {
+		if err := os.RemoveAll(outputFile); err != nil {
+			log.Fatalf("error removing file %v", err)
 		}
 	} else {
 		if err := play(voice.Body); err != nil {
@@ -206,7 +221,7 @@ func deleteFile(ctx context.Context, s3Client *s3.Client, bucket, key string) er
 func writeFile(sound io.ReadCloser, filename string) error {
 	outFile, err := os.Create(filename)
 	if err != nil {
-		return fmt.Errorf("Got error creating speech.mp3: %v", err)
+		return fmt.Errorf("error creating %s: %v", filename, err)
 	}
 
 	_, err = io.Copy(outFile, sound)
@@ -250,4 +265,63 @@ func play(sound io.ReadCloser) error {
 		return fmt.Errorf("error closing player: %w", err)
 	}
 	return nil
+}
+
+// getLength uses ffmpeg to get the duration of the audio because its not
+// returned by polly. The return value is seconds.
+func getLength(filename string) (int, error) {
+	var imagePath = EscapeFilePath(filename)
+	out, err := exec.Command("bash", "-c", fmt.Sprintf("ffmpeg -hide_banner -i %s -f null /dev/null", imagePath)).CombinedOutput()
+	if err != nil {
+		return 0, fmt.Errorf("error running ffmpeg on image: %s, error: %s, output: %s", imagePath, err.Error(), out)
+	}
+
+	// create regex and find the duration in the ffmpeg output
+	r, err := regexp.Compile(`Duration:\s\d\d:\d\d:\d\d.\d\d`)
+	if err != nil {
+		return 0, err
+	}
+	var match = r.FindString(string(out))
+	if match == "" {
+		return 0, errors.New("unable to get duration from ffmpeg")
+	}
+	match = strings.ReplaceAll(match, "Duration: ", "")
+
+	// iterate through the string and parse the time: e.g. (00:04:40.66)
+	var durationArr = strings.Split(match, ":")
+	var duration int
+	for i, digits := range durationArr {
+		switch i {
+		case 0:
+			// hours
+			num, err := strconv.Atoi(digits)
+			if err != nil {
+				return 0, err
+			}
+			duration = num * 3600
+		case 1:
+			// minutes
+			num, err := strconv.Atoi(digits)
+			if err != nil {
+				return 0, err
+			}
+			duration += num * 60
+		case 2:
+			// seconds
+			num, err := strconv.ParseFloat(digits, 32)
+			if err != nil {
+				return 0, err
+			}
+			duration += int(num)
+		default:
+			return 0, errors.New("could not parse duration")
+		}
+	}
+	return duration, nil
+}
+
+// EscapeFilePath escapes spaces in the filepath used for an exec() call
+func EscapeFilePath(file string) string {
+	var r = strings.NewReplacer(" ", `\ `, "(", `\(`, ")", `\)`, "'", `\'`, "&", `\&`, "@", `\@`)
+	return r.Replace(file)
 }
